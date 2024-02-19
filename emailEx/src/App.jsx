1a844251cf477020fb3d33fb0d35d43aba9e36b2
Gayan65 <gayan65728@yahoo.com>
2024-02-19 12:10:19 +0200

    add routes

--- emailEx/src/App.jsx
+++ emailEx/src/App.jsx
@@ -1,7 +1,22 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Blogs from "./components/Blogs";
+import NavBar from "./components/NavBar";
+
 function App() {
     return (
         <>
-            <h1>Hello</h1>
+            <h1>Hi</h1>
+            <BrowserRouter>
+                <NavBar />
+                <Routes>
+                    <Route path="/home" element={<Home />}></Route>
+                    <Route path="/" element={<Contact />}></Route>
+                    <Route path="/login" element={<Blogs />}></Route>
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
