f06b6983b99b211040a7b2f847b72ad82a73c3eb
Gayan65 <gayan65728@yahoo.com>
2024-02-19 15:01:01 +0200

    Blogs page add

--- emailEx/src/components/Blogs.jsx
+++ emailEx/src/components/Blogs.jsx
@@ -1,7 +1,69 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Accordion from "react-bootstrap/Accordion";
 
 const Blogs = () => {
-    return <div>Blogs Page</div>;
+    return (
+        <Container className="mt-5">
+            <Card>
+                <Card.Header as="h5">Blogs</Card.Header>
+                <Card.Body>
+                    <Card.Title>Blog Articles</Card.Title>
+                    <Card.Text>
+                        Find the latest articles and blog post here.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+
+            <Accordion defaultActiveKey="0" className="mt-5">
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Accordion Item # 1</Accordion.Header>
+                    <Accordion.Body>
+                        <Card>
+                            <Card.Header>Card Header # 1 </Card.Header>
+                            <Card.Body>
+                                <Card.Text>
+                                    Here's some content for the second blog
+                                    article...
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Accordion Item #2</Accordion.Header>
+                    <Accordion.Body>
+                        <Card>
+                            <Card.Header>Card Header # 2</Card.Header>
+                            <Card.Body>
+                                <Card.Text>
+                                    Here's some content for the second blog
+                                    article...
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="3">
+                    <Accordion.Header>Accordion Item #3</Accordion.Header>
+                    <Accordion.Body>
+                        <Card>
+                            <Card.Header>Card Header # 3</Card.Header>
+                            <Card.Body>
+                                <Card.Text>
+                                    Here's some content for the second blog
+                                    article...
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+        </Container>
+    );
 };
 
 export default Blogs;
--- emailEx/src/components/Home.jsx
+++ emailEx/src/components/Home.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
 
 const Home = () => {
-    return <div>Home page</div>;
+    return (
+        <Container className="mt-5">
+            <Card>
+                <Card.Header as="h5">Welcome</Card.Header>
+                <Card.Body>
+                    <Card.Title>Home Page</Card.Title>
+                    <Card.Text>
+                        This is the homepage of our website, Explore other
+                        sections using the navigation above.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <Form className="mt-5">
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        Subscribe to our newsletter for updates.
+                    </Form.Text>
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Subscribe
+                </Button>
+            </Form>
+        </Container>
+    );
 };
 
 export default Home;
