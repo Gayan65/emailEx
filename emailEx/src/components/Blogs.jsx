5cd3f8640946ba6945f93bba0d30b2564adec9e8
Gayan65 <gayan65728@yahoo.com>
2024-02-19 15:11:15 +0200

    Contact add

--- emailEx/src/components/Blogs.jsx
+++ emailEx/src/components/Blogs.jsx
@@ -1,8 +1,5 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
 import Accordion from "react-bootstrap/Accordion";
 
 const Blogs = () => {
--- emailEx/src/components/Contact.jsx
+++ emailEx/src/components/Contact.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
 
 const Contact = () => {
-    return <div>Contact Page</div>;
+    return (
+        <Container className="mt-5">
+            <Card>
+                <Card.Header as="h5">Contact me</Card.Header>
+                <Card.Body>
+                    <Card.Text>
+                        For questions or feedback, please choose a category
+                        below:
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card className="mt-3">
+                <Card.Header>General Inquiry</Card.Header>
+                <Card.Body>
+                    <Card.Text>
+                        For general inquiries, please email us at:
+                    </Card.Text>
+                    <Button variant="primary">general@example.com</Button>
+                </Card.Body>
+            </Card>
+            <Card className="mt-3">
+                <Card.Header>Feedback</Card.Header>
+                <Card.Body>
+                    <Card.Text>
+                        We value your feedback! Please share your comments at:
+                    </Card.Text>
+                    <Button variant="warning">feedback@example.com</Button>
+                </Card.Body>
+            </Card>
+            <Card className="mt-3">
+                <Card.Header>Support</Card.Header>
+                <Card.Body>
+                    <Card.Text>
+                        Need assistance! Contact our support team at:
+                    </Card.Text>
+                    <Button variant="secondary">support@example.com</Button>
+                </Card.Body>
+            </Card>
+        </Container>
+    );
 };
 
 export default Contact;
